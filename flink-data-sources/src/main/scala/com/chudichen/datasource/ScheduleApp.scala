8f0bd2da588f61a4a5f1cd0d84c252b11a54bc3a
chudichen <chudichen@foxmail.com>
2020-11-19 15:55:02 +0800

    添加sql

---
+++ flink-data-sources/src/main/scala/com/chudichen/datasource/ScheduleApp.scala
@@ -0,0 +1,15 @@
+package com.chudichen.datasource
+
+/**
+ * 定时警告
+ *
+ * @author chudichen
+ * @since 2020-11-19
+ */
+class ScheduleApp {
+
+  def main(args: Array[String]): Unit = {
+    // 定时捞取规则，每隔一分钟捞取一次
+
+  }
+}
